9a2c897dc8ed925978d42bf3f4949c746781d0cc
Your Name <you@example.com>
2023-04-14 01:30:57 +0300

    create function safe_all_images, rename "id"

--- get_spacex_images.py
+++ get_spacex_images.py
@@ -6,13 +6,13 @@ from work_with_files import define_filetype
 from pathlib import Path
 
 
-def get_urls_spacex_images_by_id(id):
+def get_urls_spacex_images_by_id(launch_id):
     response = requests.get("https://api.spacexdata.com/v5/launches")
     response.raise_for_status()
-    for element in response.json():
-        if element['id'] == id:
-            image_urls = element["links"]["flickr"]["original"]
-            date = element["date_local"]
+    for launch_inf in response.json():
+        if launch_inf['id'] == launch_id:
+            image_urls = launch_inf["links"]["flickr"]["original"]
+            date = launch_inf["date_local"]
             break
     return image_urls, date
 
@@ -20,13 +20,14 @@ def get_urls_spacex_images_by_id(id):
 def get_urls_spacex_images_by_launch_num(launch_num):
     response = requests.get("https://api.spacexdata.com/v5/launches")
     response.raise_for_status()
-    image_urls = response.json()[launch_num]["links"]["flickr"]["original"]
-    date = response.json()[launch_num]["date_local"]
+    json_response = response.json()
+    image_urls = json_response[launch_num]["links"]["flickr"]["original"]
+    date = json_response[launch_num]["date_local"]
     return image_urls, date
 
 
 
-def get_last_id_with_images():
+def get_last_launch_id_with_images():
     launch_num = -1
     while True:
         response = requests.get(
@@ -42,6 +43,16 @@ def get_last_id_with_images():
     return last_id, launch_num
 
 
+def safe_all_images(image_urls, date, launch_id):
+    os.makedirs('Images_spacex', exist_ok=True)
+    for num_image_url, image_url in enumerate(image_urls):
+        safe_images(
+            image_url,
+            Path.cwd() / 'Images_spacex' / f'spacex_{launch_id}_{date[0:10]}_{num_image_url}'
+                                           f'{define_filetype(image_url)}'
+        )
+
+
 def main():
     parser = argparse.ArgumentParser(
         description="Программа загружает кадры запуска Spacex"
@@ -55,23 +66,11 @@ def main():
     args = parser.parse_args()
     if args.id:
         image_urls, date = get_urls_spacex_images_by_id(args.id)
-        os.makedirs('Images_spacex', exist_ok=True)
-        for num_image_url, image_url in enumerate(image_urls):
-            safe_images(
-                image_url,
-                Path.cwd() / 'Images_spacex' / f'spacex_{args.id}_{date[0:10]}_{num_image_url}'
-                                               f'{define_filetype(image_url)}'
-            )
+        safe_all_images(image_urls, date, args.id)
     else:
-        last_id, launch_nam = get_last_id_with_images()
-        image_urls, date = get_urls_spacex_images_by_launch_num(launch_nam)
-        os.makedirs('Images_spacex', exist_ok=True)
-        for num, image_url in enumerate(image_urls):
-            safe_images(
-                image_url,
-                Path.cwd() / 'Images_spacex' / f'spacex_{last_id}_{date[0:10]}_{num}'
-                                               f'{define_filetype(image_url)}'
-            )
+        last_id, launch_num = get_last_launch_id_with_images()
+        image_urls, date = get_urls_spacex_images_by_launch_num(launch_num)
+        safe_all_images(image_urls, date, last_id)
 
 if __name__ == "__main__":
     main()
