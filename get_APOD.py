00f7d28601d4f71135742681ff7e3cc4ddd7abad
Your Name <you@example.com>
2023-04-21 20:11:48 +0300

    create check for the existence of an image

--- get_APOD.py
+++ get_APOD.py
@@ -2,7 +2,7 @@ import os
 import argparse
 import requests
 from dotenv import load_dotenv
-from work_with_files import safe_images
+from work_with_files import save_images
 from work_with_files import define_filetype
 from pathlib import Path
 
@@ -15,17 +15,20 @@ def get_APOD(params, day=None):
     )
     response.raise_for_status()
     os.makedirs('Photos_of_the_day', exist_ok=True)
-    json_response = response.json()
+    response_dictionaries_in_list = response.json()
     if day:
-        json_response = [json_response]
-    for dict_in_list in json_response:
-        try:
-            url = dict_in_list['hdurl']
-        except KeyError:
-            url = dict_in_list['url']
+        response_dictionaries_in_list = [response_dictionaries_in_list]
+    for dictionary_in_list in response_dictionaries_in_list:
+        date = dictionary_in_list['date']
+        if dictionary_in_list['media_type'] == "image":
+            try:
+                url = dictionary_in_list['hdurl']
+            except KeyError:
+                url = dictionary_in_list['url']
+        else:
+            print(f'За дату {date} нет изображения')
         filetype = define_filetype(url)
-        date = dict_in_list['date']
-        safe_images(
+        save_images(
             url,
             Path.cwd() / 'Photos_of_the_day' / f'image_for_{date}{filetype}'
         )
@@ -36,11 +39,25 @@ def main():
     nasa_api = os.environ['NASA_API']
     parser = argparse.ArgumentParser(
         description='Программа скачивает фотографии дня в файл '
-                    '"Photos_of_the_day" в рабочей директории, (если нет создает)'
+                    '"Photos_of_the_day" в рабочей директории,'
+                    ' (если нет создает)'
     )
-    parser.add_argument("--count", type=int,  help="Укажите количество случайных фотографий")
-    parser.add_argument("--day", help="Укажите день, за который нужно получить фото, в формате YYYY-MM-DD")
-    parser.add_argument("--days", help="Укажите промежуток дней, за которые хотите получить фотографии, в формате YYYY-MM-DD-YYYY-MM-DD")
+    parser.add_argument(
+        "--count",
+        type=int,
+        help="Укажите количество случайных фотографий"
+    )
+    parser.add_argument(
+        "--day",
+        help="Укажите день, за который нужно"
+             " получить фото, в формате YYYY-MM-DD"
+    )
+    parser.add_argument(
+        "--days",
+        help="Укажите промежуток дней, за которые хотите"
+             " получить фотографии,"
+             " в формате YYYY-MM-DD-YYYY-MM-DD"
+    )
     args = parser.parse_args()
     if args.count:
         params = {
